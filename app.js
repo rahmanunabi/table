e664dc252ff35b297a4a8e192251e0e33af5e41d
rahmanunabi <102538438+rahmanunabi@users.noreply.github.com>
2022-05-24 16:47:50 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+var number = prompt ("Please select any number for table");
+
+var table = number;
+var multiply = 1;
+
+document.write("<h1>" + "Table" + "</h1>");
+document.write("<h1>" + table + " " + "X" + " " + multiply + " " + "=" + " " + table * multiply + "</h1>" + "\n");
+document.write("<h1>" + table + " " + "X" + " " + ++multiply + " " + "=" + " " + table * multiply + "</h1>" + "\n");
+document.write("<h1>" + table + " " + "X" + " " + ++multiply + " " + "=" + " " + table * multiply + "</h1>" + "\n");
+document.write("<h1>" + table + " " + "X" + " " + ++multiply + " " + "=" + " " + table * multiply + "</h1>" + "\n");
+document.write("<h1>" + table + " " + "X" + " " + ++multiply + " " + "=" + " " + table * multiply + "</h1>" + "\n");
+document.write("<h1>" + table + " " + "X" + " " + ++multiply + " " + "=" + " " + table * multiply + "</h1>" + "\n");
+document.write("<h1>" + table + " " + "X" + " " + ++multiply + " " + "=" + " " + table * multiply + "</h1>" + "\n");
+document.write("<h1>" + table + " " + "X" + " " + ++multiply + " " + "=" + " " + table * multiply + "</h1>" + "\n");
+document.write("<h1>" + table + " " + "X" + " " + ++multiply + " " + "=" + " " + table * multiply + "</h1>" + "\n");
+document.write("<h1>" + table + " " + "X" + " " + ++multiply + " " + "=" + " " + table * multiply + "</h1>" + "\n");
